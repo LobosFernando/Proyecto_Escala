ec8d8f381e033a47ad779f782ae2f5044cee7d87
Fernando Lobos <lobosfernandoexequiel@gmail.com>
2022-10-28 21:22:28 -0300

    Imagenes_Requerimientos

--- index.php
+++ index.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Style/Css/style.css">
     <link rel="stylesheet" href="fontello.css">
     <script src="script.js"></script>
     <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
     <title>Escala Indumentaria</title>
 </head>
+
 <body>
     <div class="encabezado">
-           <img class="img" src="logo.png" alt="">
+        <img class="img" src="logo.png" alt="">
         <div class="nav">
             <p class="efectoSubrayado leftToRight">Inicio</p>
             <p class="efectoSubrayado leftToRight">Tienda</p>
             <p class="efectoSubrayado leftToRight">Acerca de</p>
             <p class="efectoSubrayado leftToRight">Contacto</p>
-            <a href=""><img src="carritoFinal.png" alt="" ></a>
+            <a href=""><img src="carritoFinal.png" alt=""></a>
             <button class="btnIniciarSesion">Iniciar sesión</button>
             </button>
         </div>
@@ -28,25 +30,27 @@
     <div id="carrusel-contenido">
         <div id="carrusel-caja">
             <div class="carrusel-elemento">
-                <img class="imagenes" src="imagen1.jpg" >
+                <img class="imagenes" src="imagen1.jpg">
             </div>
-            <div class="carrusel-elemento">   
+            <div class="carrusel-elemento">
                 <img class="imagenes" src="imagen2.jpg">
             </div>
-            <div class="carrusel-elemento">   
-                <img class="imagenes" src="imagen3.jpg">                        
+            <div class="carrusel-elemento">
+                <img class="imagenes" src="imagen3.jpg">
             </div>
         </div>
 
     </div>
     <a href="https://api.whatsapp.com/send?phone=543564589550" target="_blank" class="btn-wsp"> <i class="fa fa-whatsapp icono"></i></i></a>
     <div class="accesoDirectoTienda">
-        <center><h1 style="color:white ;">Comprá ahora</h1></center>
+        <center>
+            <h1 style="color:white ;">Comprá ahora</h1>
+        </center>
         <div class="tiendaMujer">
-            
+
             <a href="tiendaMujer.html"><img class="imgTiendaMujer" src="tiendamujer.jpg" alt=""></a>
             <div class="textoEncima">Ir a la tienda de mujer</div>
-            
+
         </div>
         <div class="tiendaHombre">
             <a href="tiendaHombres.html"><img class="imgTiendaHombre" src="tiendahombre.jpg" alt=""></a>
@@ -54,18 +58,19 @@
         </div>
 
     </div>
-    
+
     <div class="footer">
-        <p style="color:white; margin-left: 550px;font-size: 25px; padding-top: 25px; ">SEGUINOS EN:      <img class="imgRedes" src="image.png" alt="">          <img class="imgRedes" src="image (1).png" alt=""> </p>
+        <p style="color:white; margin-left: 550px;font-size: 25px; padding-top: 25px; ">SEGUINOS EN: <img class="imgRedes" src="image.png" alt=""> <img class="imgRedes" src="image (1).png" alt=""> </p>
         <div class="contieneEnlaces">
-            <a class="enlacesFooter" href="">SOBRE NOSOTROS</a> 
-            <a class="enlacesFooter"href="">PREGUNTAS FRECUENTES</a>
-            <a class="enlacesFooter"href="">PAGO Y ENVÍO</a>
-            <a class="enlacesFooter"href="">POLÍTICAS DE CAMBIO</a>
+            <a class="enlacesFooter" href="">SOBRE NOSOTROS</a>
+            <a class="enlacesFooter" href="">PREGUNTAS FRECUENTES</a>
+            <a class="enlacesFooter" href="">PAGO Y ENVÍO</a>
+            <a class="enlacesFooter" href="">POLÍTICAS DE CAMBIO</a>
 
         </div>
-        
+
     </div>
-    
+
 </body>
+
 </html>
