d5ca4f8ae4401fa4e8bc598e83c794dfb89b14c3
VincentiJulieta <vincentijuli@gmail.com>
2022-11-19 16:23:00 -0300

    cambios en boton perfil

--- encabezado.php
+++ encabezado.php
@@ -1,4 +1,8 @@
 <html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </head>
 <?php session_start(); ?>
 <div class="encabezado">
         <img class="img" src="Assets/Imágenes/logo.png" alt="">
@@ -9,7 +13,17 @@
             <a href=""><img src="Assets/Imágenes/carritoFinal.png" alt="" ></a>
             <?php 
             if(isset($_SESSION['usuario'])) { ?>
-            <button class="btnIniciarSesion"> <a href="perfil.php"><?php echo $_SESSION['usuario']?></a></button>
+  
+  <div class="btn-group" role="group">
+    <button  type="button" class="btnIniciarSesion" data-bs-toggle="dropdown" >
+    <?php echo $_SESSION['usuario']?>
+    </button>
+    <ul class="dropdown-menu" 
+      <li><a class="dropdown-item" href="perfil.php">Editar perfil</a></li>
+      <li><a class="dropdown-item" href="logout.php">Cerrar sesión</a></li>
+    </ul>
+  </div>
+</div>
             <?php } else { ?>
                 <button class="btnIniciarSesion"> <a href="login.php">Iniciar sesión</a></button>
                 <?php }?>
