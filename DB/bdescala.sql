615cca8fe8b847472442ab5707b888d1ab38318b
VincentiJulieta <vincentijuli@gmail.com>
2023-02-23 00:15:29 -0300

    hbj

--- DB/bdescala.sql
+++ DB/bdescala.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2022 a las 02:35:40
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 23-02-2023 a las 04:14:17
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,6 +24,99 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `idCarrito` int(11) NOT NULL,
+  `producto` varchar(30) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precio` float NOT NULL,
+  `subtotal` float NOT NULL,
+  `usuario` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `carrito`
+--
+
+INSERT INTO `carrito` (`idCarrito`, `producto`, `color`, `cantidad`, `precio`, `subtotal`, `usuario`) VALUES
+(133, 'Remera Hola', '#1f6d93', 0, 19990, 0, 'Ju1234'),
+(134, 'Remera Chau', '#139563', 2, 3590, 7180, 'Ju1234'),
+(135, 'Remera Hola', '#c60c0c', 1, 19990, 19990, 'Ju1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoriashombre`
+--
+
+CREATE TABLE `categoriashombre` (
+  `idcategoriah` int(11) NOT NULL,
+  `nombrecath` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoriashombre`
+--
+
+INSERT INTO `categoriashombre` (`idcategoriah`, `nombrecath`) VALUES
+(1, 'Remeras'),
+(2, 'Jeans'),
+(3, 'Bermudas'),
+(4, 'Abrigos');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoriasmujer`
+--
+
+CREATE TABLE `categoriasmujer` (
+  `idCategoriam` int(11) NOT NULL,
+  `nombreCatM` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoriasmujer`
+--
+
+INSERT INTO `categoriasmujer` (`idCategoriam`, `nombreCatM`) VALUES
+(1, 'RemerasM'),
+(2, 'JeansM'),
+(3, 'ShortsM'),
+(4, 'AbrigosM'),
+(7, 'TopsM');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalleproducto`
+--
+
+CREATE TABLE `detalleproducto` (
+  `idDetalleProducto` int(11) NOT NULL,
+  `producto` varchar(30) NOT NULL,
+  `color` varchar(30) NOT NULL,
+  `talle` varchar(10) NOT NULL,
+  `cant` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalleproducto`
+--
+
+INSERT INTO `detalleproducto` (`idDetalleProducto`, `producto`, `color`, `talle`, `cant`) VALUES
+(36, 'Remera Hola', '#c60c0c', 'S', 1),
+(37, 'Remera Hola', '#c60c0c', 'XL', 1),
+(38, 'Remera Hola', '#1f6d93', 'M', 1),
+(39, 'Remera Hola', '#1f6d93', '3XL', 1),
+(40, 'Remera Chau', '#139563', 'L', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `localidades`
 --
 
@@ -31,7 +124,7 @@ CREATE TABLE `localidades` (
   `idLocalidad` int(11) NOT NULL,
   `nombreLocalidad` varchar(20) DEFAULT NULL,
   `idProvincia` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `localidades`
@@ -43,17 +136,24 @@ INSERT INTO `localidades` (`idLocalidad`, `nombreLocalidad`, `idProvincia`) VALU
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `productos`
+-- Estructura de tabla para la tabla `prod`
 --
 
-CREATE TABLE `productos` (
-  `codProducto` int(11) NOT NULL,
-  `nombreProducto` varchar(50) NOT NULL,
-  `precio` float NOT NULL,
-  `stock` int(11) NOT NULL,
-  `comentarios` varchar(300) DEFAULT NULL,
-  `idSexo` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `prod` (
+  `codProd` int(11) NOT NULL,
+  `nombreProd` varchar(30) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `descripcion` varchar(300) NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `prod`
+--
+
+INSERT INTO `prod` (`codProd`, `nombreProd`, `categoria`, `descripcion`, `precio`) VALUES
+(41, 'Remera Hola', 'RemerasM', 'Remera bordada ', 19990),
+(42, 'Remera Chau', 'RemerasM', 'etd', 3590);
 
 -- --------------------------------------------------------
 
@@ -64,7 +164,7 @@ CREATE TABLE `productos` (
 CREATE TABLE `provincias` (
   `idProvincia` int(11) NOT NULL,
   `nombreProvincia` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `provincias`
@@ -82,7 +182,7 @@ INSERT INTO `provincias` (`idProvincia`, `nombreProvincia`) VALUES
 CREATE TABLE `roles` (
   `idRol` int(11) NOT NULL,
   `tipoRol` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -93,7 +193,79 @@ CREATE TABLE `roles` (
 CREATE TABLE `sexo` (
   `idSexo` int(2) NOT NULL,
   `nombreSexo` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `sexo`
+--
+
+INSERT INTO `sexo` (`idSexo`, `nombreSexo`) VALUES
+(1, 'Hombre'),
+(2, 'Mujer');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `talles`
+--
+
+CREATE TABLE `talles` (
+  `idTalle` int(11) NOT NULL,
+  `talle` varchar(10) NOT NULL,
+  `idTipo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `talles`
+--
+
+INSERT INTO `talles` (`idTalle`, `talle`, `idTipo`) VALUES
+(7, 'S', 1),
+(8, 'M', 1),
+(9, 'L', 1),
+(10, 'XL', 1),
+(11, 'XXL', 1),
+(12, '3XL', 1),
+(13, '4XL', 1),
+(14, '5XL', 1),
+(16, '36', 3),
+(17, '38', 3),
+(18, '40', 3),
+(19, '42', 3),
+(20, '44', 3),
+(21, '46', 3),
+(22, '48', 3),
+(23, '50', 3),
+(24, '52', 3),
+(25, '54', 3),
+(26, '56', 3),
+(27, '39', 4),
+(28, '40', 4),
+(29, '41', 4),
+(30, '42', 4),
+(31, '43', 4),
+(32, '44', 4),
+(33, '45', 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipostalles`
+--
+
+CREATE TABLE `tipostalles` (
+  `idTipo` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipostalles`
+--
+
+INSERT INTO `tipostalles` (`idTipo`, `tipo`) VALUES
+(1, 'S/M/L/XL/XXL/3XL/4XL/5XL'),
+(3, '36/38/40/42/44/46/48/50/52/54/56'),
+(4, '39/40/41/42/43/44/45');
 
 -- --------------------------------------------------------
 
@@ -113,7 +285,16 @@ CREATE TABLE `usuarios` (
   `telefono` int(11) DEFAULT NULL,
   `idRol` int(11) DEFAULT NULL,
   `contraseña` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`idUsuario`, `nombreDeUsuario`, `nombre`, `apellido`, `correoElectronico`, `dni`, `domicilio`, `idLocalidad`, `telefono`, `idRol`, `contraseña`) VALUES
+(4, 'Ju1234', 'Ju', NULL, 'ju@gmail.com', NULL, NULL, NULL, NULL, NULL, '123'),
+(5, 'thiago124', 'Thiago', NULL, 'thiago@gmail.com', NULL, NULL, NULL, NULL, NULL, '1234'),
+(6, 'dana123', 'Dana', NULL, 'dana@gmail.com', NULL, NULL, NULL, NULL, NULL, '1234');
 
 -- --------------------------------------------------------
 
@@ -125,23 +306,35 @@ CREATE TABLE `ventas` (
   `nroVenta` int(11) NOT NULL,
   `nombreDeUsuario` varchar(20) DEFAULT NULL,
   `montoTotal` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ventasproductos`
---
-
-CREATE TABLE `ventasproductos` (
-  `idVentasProductos` int(11) NOT NULL,
-  `codProducto` int(11) DEFAULT NULL,
-  `nroVenta` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`idCarrito`);
+
+--
+-- Indices de la tabla `categoriashombre`
+--
+ALTER TABLE `categoriashombre`
+  ADD PRIMARY KEY (`idcategoriah`);
+
+--
+-- Indices de la tabla `categoriasmujer`
+--
+ALTER TABLE `categoriasmujer`
+  ADD PRIMARY KEY (`idCategoriam`);
+
+--
+-- Indices de la tabla `detalleproducto`
+--
+ALTER TABLE `detalleproducto`
+  ADD PRIMARY KEY (`idDetalleProducto`);
 
 --
 -- Indices de la tabla `localidades`
@@ -151,10 +344,10 @@ ALTER TABLE `localidades`
   ADD KEY `FK_LocalidadesProvincias` (`idProvincia`);
 
 --
--- Indices de la tabla `productos`
+-- Indices de la tabla `prod`
 --
-ALTER TABLE `productos`
-  ADD PRIMARY KEY (`codProducto`);
+ALTER TABLE `prod`
+  ADD PRIMARY KEY (`codProd`);
 
 --
 -- Indices de la tabla `provincias`
@@ -175,6 +368,18 @@ ALTER TABLE `sexo`
   ADD PRIMARY KEY (`idSexo`);
 
 --
+-- Indices de la tabla `talles`
+--
+ALTER TABLE `talles`
+  ADD PRIMARY KEY (`idTalle`);
+
+--
+-- Indices de la tabla `tipostalles`
+--
+ALTER TABLE `tipostalles`
+  ADD PRIMARY KEY (`idTipo`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -191,16 +396,32 @@ ALTER TABLE `ventas`
   ADD KEY `FK_VentasUsuario` (`nombreDeUsuario`);
 
 --
--- Indices de la tabla `ventasproductos`
---
-ALTER TABLE `ventasproductos`
-  ADD PRIMARY KEY (`idVentasProductos`),
-  ADD KEY `FK_ventasProductos_Productos` (`codProducto`),
-  ADD KEY `FK_ventasProductos_Ventas` (`nroVenta`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  MODIFY `idCarrito` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+
+--
+-- AUTO_INCREMENT de la tabla `categoriashombre`
+--
+ALTER TABLE `categoriashombre`
+  MODIFY `idcategoriah` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `categoriasmujer`
+--
+ALTER TABLE `categoriasmujer`
+  MODIFY `idCategoriam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de la tabla `detalleproducto`
+--
+ALTER TABLE `detalleproducto`
+  MODIFY `idDetalleProducto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT de la tabla `localidades`
@@ -209,10 +430,10 @@ ALTER TABLE `localidades`
   MODIFY `idLocalidad` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `productos`
+-- AUTO_INCREMENT de la tabla `prod`
 --
-ALTER TABLE `productos`
-  MODIFY `codProducto` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `prod`
+  MODIFY `codProd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
 -- AUTO_INCREMENT de la tabla `provincias`
@@ -230,25 +451,31 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT de la tabla `sexo`
 --
 ALTER TABLE `sexo`
-  MODIFY `idSexo` int(2) NOT NULL AUTO_INCREMENT;
+  MODIFY `idSexo` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `talles`
+--
+ALTER TABLE `talles`
+  MODIFY `idTalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT de la tabla `tipostalles`
+--
+ALTER TABLE `tipostalles`
+  MODIFY `idTipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
   MODIFY `nroVenta` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `ventasproductos`
---
-ALTER TABLE `ventasproductos`
-  MODIFY `idVentasProductos` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Restricciones para tablas volcadas
@@ -259,6 +486,12 @@ ALTER TABLE `ventasproductos`
 --
 ALTER TABLE `localidades`
   ADD CONSTRAINT `FK_LocalidadesProvincias` FOREIGN KEY (`idProvincia`) REFERENCES `provincias` (`idProvincia`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `talles`
+--
+ALTER TABLE `talles`
+  ADD CONSTRAINT `talles_ibfk_1` FOREIGN KEY (`idTipo`) REFERENCES `tipostalles` (`idTipo`);
 
 --
 -- Filtros para la tabla `usuarios`
@@ -272,13 +505,6 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `ventas`
   ADD CONSTRAINT `FK_VentasUsuario` FOREIGN KEY (`nombreDeUsuario`) REFERENCES `usuarios` (`nombreDeUsuario`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `ventasproductos`
---
-ALTER TABLE `ventasproductos`
-  ADD CONSTRAINT `FK_ventasProductos_Productos` FOREIGN KEY (`codProducto`) REFERENCES `productos` (`codProducto`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `FK_ventasProductos_Ventas` FOREIGN KEY (`nroVenta`) REFERENCES `ventas` (`nroVenta`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
